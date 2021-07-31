026813416b2c77fc19176cae9a8737277e0641ef
Wladut <vrinceanu.ioan.vladut@gmail.com>
2021-07-31 23:43:18 +0300

    LD-22 #comment update

--- c/05/05_01.c
+++ c/05/05_01.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include "05_01.h"
 
 #define COUNT 10
 #define GREETING "Hello! "
---
+++ c/05/05_02.c
@@ -0,0 +1,16 @@
+/* header files are not the same as  libraries
+   headers contain function prototypes, definitions, macros and so on
+   comiler warning appears if header is  missing
+
+   libraries -> provide common c language functions, missing libraries
+   generate linker errors
+
+    Linker -> clang 01_01-math.c -lm 
+              - clang : compiler name
+              - 01_01-math.c : source file name
+              - -lm : l -> link and m -> math library on the system
+
+              GCC compiler:
+              gcc file_name.c -o file_name -> compile
+              .\file_name -> run exe
+*/
