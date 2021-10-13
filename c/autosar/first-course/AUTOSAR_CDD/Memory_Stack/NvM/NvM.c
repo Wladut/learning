af8105ddc44af2898604abcc9c7a6ced430fca8b
Wladut <vrinceanu.ioan.vladut@gmail.com>
2021-10-13 11:06:03 +0300

    LD-41 finish Memory Stack and AUTOSAR CDD 1h 44m

---
+++ c/autosar/first-course/AUTOSAR_CDD/Memory_Stack/NvM/NvM.c
@@ -0,0 +1,31 @@
+/*
+ * NvM.c
+ *
+ *  Created on: 22 feb. 2019
+ *      Author: Suciu Mihai
+ */
+
+#include "NvM_Types.h"
+
+Std_ReturnType NvM_WriteBlock(NvM_BlockIdType Block_Id,const void * Data )
+{
+   return E_OK;
+}
+Std_ReturnType NvM_ReadBlock(NvM_BlockIdType Block_Id, void * Data )
+{
+   return E_OK;
+}
+
+/*  Home Work:
+ *  Define the interfaces: NvM_GetErrorStatus(), NvM_Init(), NvM_ReadAll(), NvM_WriteAll(),NvM_SetRamBlockStatus()
+ *
+ *
+ * */
+
+/* Starting Home Work */
+
+
+
+
+/* Ending Home Work */
+
---
+++ c/autosar/first-course/AUTOSAR_CDD/Memory_Stack/NvM/NvM.h
@@ -0,0 +1,15 @@
+/*
+ * NvM.h
+ *
+ *  Created on: 22 feb. 2019
+ *      Author: Suciu Mihai
+ */
+
+#ifndef MEMORY_STACK_NVM_NVM_H_
+#define MEMORY_STACK_NVM_NVM_H_
+
+Std_ReturnType NvM_ReadBlock(uint16 Block_Id,const void * Data );
+Std_ReturnType NvM_ReadBlock(uint16 Block_Id, void * Data );
+
+
+#endif /* MEMORY_STACK_NVM_NVM_H_ */
