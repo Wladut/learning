70443c8ea3b48d535f02d0e42ca7a7e7d3705059
Wladut <vrinceanu.ioan.vladut@gmail.com>
2022-02-13 19:48:15 +0200

    LD-44 #message added demo for cmake

---
+++ cmake/cmake-commandline/build/CMakeFiles/mainExec.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainExec.dir/main.cpp.obj"
+  "CMakeFiles/mainExec.dir/main.cpp.obj.d"
+  "libmainExec.dll.a"
+  "mainExec.exe"
+  "mainExec.exe.manifest"
+  "mainExec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainExec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake/demo-vscode/demo-vscode/demo-vscode.h
@@ -0,0 +1,8 @@
+﻿// demo-vscode.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
